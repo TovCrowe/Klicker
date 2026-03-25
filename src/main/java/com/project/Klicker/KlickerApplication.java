01e6656b694798b0465c2d1b9a88b9ac83cf9806
ToviP <manuelxdvic@gmail.com>
2026-03-25 14:07:16 -0600

    Project start

---
+++ src/main/java/com/project/Klicker/KlickerApplication.java
@@ -0,0 +1,13 @@
+package com.project.Klicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Klicker/KlickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Klicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
